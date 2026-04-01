305e2b4be2233034be00474b1ebb3e41f0e51ff9
Dharma Teja <143797004+Dharmateja69@users.noreply.github.com>
2026-04-01 11:09:43 +0530

    Add: special-array-i - submission-1

---
+++ Data Structures & Algorithms/special-array-i/submission-1.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        if(nums.length==1){
+            return true;
+        }
+        boolean previous_pair_flag = false;
+      for(int i = 0;i<nums.length-1;i++)
+      {    //if 1st is even then 
+           if(nums[i]%2==0 && nums[i+1]%2==0 || nums[i]%2!=0 && nums[i+1]%2!=0){
+                return false;
+           }
+          
+           
+         previous_pair_flag = false ;
+           
+      }
+
+    return true;
+    }
+}
